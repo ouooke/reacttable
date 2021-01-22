ada1e603429c728123919428859231c30b13f44e
ywang <ywang@unisrv.jp>
2021-01-23 03:31:17 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import * as ReactBootStrap from "react-bootstrap";
+import textFactory, { textFilter } from "react-bootstrap-table2-filter";
+import dateFactory, { dateFilter } from "react-bootstrap-table2-filter";
 
-function App() {
+const App = () => {
+  const [players, setPlayers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const getPlayerData = async () => {
+    try {
+      const data = await axios.get(
+        "https://nba-players.herokuapp.com/players-stats"
+      );
+      console.log(data);
+      setPlayers(data.data);
+      setLoading(true);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const columns = [
+    { dataField: "name", text: "Player Name", sort: true },
+    { dataField: "points_per_game", text: "Points Per Game", sort: true, filter: dateFilter() },
+    { dataField: "games_played", text: "games_played", sort: true },
+    { dataField: "assists_per_game", text: "assists_per_game", filter: textFilter() },
+    { dataField: "minutes_per_game", text: "minutes_per_game" },
+    { dataField: "turnovers_per_game", text: "turnovers_per_game" },
+  ];
+
+
+  useEffect(() => {
+    getPlayerData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <BootstrapTable
+          keyField="name"
+          data={players}
+          columns={columns}
+          pagination={paginationFactory()}
+          filter={textFactory()}
+          filter={dateFactory()}
+        />
+      ) : (
+          <ReactBootStrap.Spinner animation="border" />
+        )}
     </div>
   );
-}
+};
 
 export default App;
