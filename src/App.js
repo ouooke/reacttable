2196a0a51f990c777a166e6c6e490351279853ff
alotofwool <76927917+ouooke@users.noreply.github.com>
2021-01-24 21:08:31 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import axios from "axios";
 import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory from "react-bootstrap-table2-paginator";
 import * as ReactBootStrap from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 import textFactory, { textFilter } from "react-bootstrap-table2-filter";
-import dateFactory, { dateFilter } from "react-bootstrap-table2-filter";
+import dateFactory, { dateFilter, selectFilter } from "react-bootstrap-table2-filter";
 
 const App = () => {
   const [dailythirtyflashreport, setdailythirtyflashreport] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [selectRow, setSelectRow] = useState(null);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
   const getdailythirtyflashreportData = async () => {
     try {
       const data = await axios.get(
@@ -22,22 +26,39 @@ const App = () => {
       console.log(e);
     }
   };
+  const selectOptions = {
+    0: "低圧",
+    1: "高圧"
+  };
+
   const columns = [
     { dataField: "infomationclassificationcode", text: "情報区分コード", sort: true },
-    { dataField: "sendercode", text: "送信者コード", filter: textFilter(), sort: true},
+    { dataField: "sendercode", filter: textFilter(),text: "送信者コード",  sort: true },
     { dataField: "sendername", text: "送信者名称", sort: true },
     { dataField: "receivercode", text: "受信者コード", filter: textFilter() },
     { dataField: "receivername", text: "受信者名称" },
     { dataField: "filecreatedate", text: "ファイル作成年月日" },
     { dataField: "filecreatetime", text: "ファイル作成時分" },
-    { dataField: "createddate", text: "取得年月日", filter: dateFilter()  },
-    { dataField: "supplypointspecificnum", text: "供給地点特定番号", filter: textFilter() },
+    { dataField: "createddate", text: "取得年月日", filter: dateFilter() },
     { dataField: "customerid", text: "需要家識別番号" },
+    { dataField: "supplypointspecificnum", text: "供給地点特定番号", filter: textFilter() },
     { dataField: "customername", text: "需要家名" },
     { dataField: "managementnum", text: "管理番号", filter: textFilter() },
-    { dataField: "voltageflg", text: "電圧区分" },
+    {
+      dataField: "voltageflg", text: "電圧区分", formatter: cell => selectOptions[cell],
+      filter: selectFilter({
+        options: selectOptions
+      })
+    },
   ];
 
+  const rowEvents = {
+    onClick: (e, row, rowIndex) => {
+      console.log(row, 79809)
+      setSelectRow(row);
+      setShow(true);
+    }
+  };
 
   useEffect(() => {
     getdailythirtyflashreportData();
@@ -45,6 +66,23 @@ const App = () => {
 
   return (
     <div className="App">
+      {/* modal */}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          {/* <p>{(typeof(selectRow) === "object") && selectRow.sendername}</p> */}
+          <p>8989</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary"onClick={handleClose}>Close</Button>
+          <Button variant="primary">ok</Button>
+        </Modal.Footer>
+      </Modal>
+
       {loading ? (
         <BootstrapTable
           keyField="name"
@@ -53,6 +91,7 @@ const App = () => {
           pagination={paginationFactory()}
           filter={textFactory()}
           filter={dateFactory()}
+          rowEvents={rowEvents}
         />
       ) : (
           <ReactBootStrap.Spinner animation="border" />
